cf0dcc42f5601fce7cf2388ddfde0d50ad55156b
Jim Benton <jim@autonomousmachine.com>
2011-05-28 15:35:00 -0500

    Add 1.7

---
+++ 1/1.7.scm
@@ -0,0 +1,34 @@
+; SICP Exercise 1.2
+
+(define (puts x)
+  (display x)
+  (newline))
+
+(define (square x)
+  (* x x ))
+
+(define (sqrt-iter last-guess guess x)
+  (if (good-enough? last-guess guess)
+    guess
+    (sqrt-iter guess (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? last-guess guess)
+  (< (/ (abs (- last-guess guess)) guess) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 0 1.0 x))
+
+; basic
+(puts (sqrt 25))
+
+; small number, was inaccurate with original method
+(puts (sqrt .0000025))
+
+; large number, would hang with original method
+(puts (sqrt 250000000000000))
